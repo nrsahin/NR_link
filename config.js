0efa3b92a6056bec4ab448383bcb844bc6a71a0d
Najibur Rahaman <156786346+nrsahin@users.noreply.github.com>
2026-02-11 21:25:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 // config.js
 window.CONFIG = {
-  API_BASE: "http://localhost:5000/api",
+  API_BASE: "https://server-1-o83g.onrender.com/api",
   // API_BASE: "https://inkjet-exhibition-device-acts.trycloudflare.com/api"
 };
+
