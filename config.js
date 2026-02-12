b6cea2d6f0ccffd2ad421ce55d1907aae1639cf4
Najibur Rahaman <156786346+nrsahin@users.noreply.github.com>
2026-02-12 13:34:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // config.js
 window.CONFIG = {
-  API_BASE: "https://server-1-o83g.onrender.com/api",
+  API_BASE: "https://server--messnazrul.replit.app/api",
   // API_BASE: "https://inkjet-exhibition-device-acts.trycloudflare.com/api"
 };
+
 
